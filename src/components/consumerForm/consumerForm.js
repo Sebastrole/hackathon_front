0db3256dba72d867166093ec0273c75a61c98f5a
Sebastrole <sebaspaloma@hotmail.com>
2021-09-25 17:40:12 -0500

    tercer commit, audio implementado

--- src/components/consumerForm/consumerForm.js
+++ src/components/consumerForm/consumerForm.js
@@ -1,9 +1,63 @@
-function consumerForm() {
-  return (
-    <div>
-        Hola world
-    </div>
-  );
+import "./consumerForm.css";
+import MicRecorder from 'mic-recorder-to-mp3';
+import { useState } from "react";
+const Mp3Recorder = new MicRecorder({ bitRate: 128 });
+
+const ConsumerForm = () => {
+    const [isRecording,setIsRecording]= useState(false)
+    const [blobURL,setblobURL]= useState("")
+    const [isBlocked,setisBlocked]= useState(false)
+
+    navigator.getUserMedia({ audio: true },
+        () => {
+            console.log('Permission Granted');
+            setisBlocked(false);
+        },
+        () => {
+            console.log('Permission Denied');
+            setisBlocked(true);
+        },
+    );
+    
+    const start = () => {
+        if (isBlocked) {
+          console.log('Permission Denied');
+        } else {
+          Mp3Recorder
+            .start()
+            .then(() => {
+              setIsRecording(true);
+            }).catch((e) => console.error(e));
+        }
+      };
+    
+    const stop = () => {
+        Mp3Recorder
+          .stop()
+          .getMp3()
+          .then(([buffer, blob]) => {
+            const blobURL = URL.createObjectURL(blob)
+            setIsRecording(false);
+            setblobURL(blobURL)
+          }).catch((e) => console.log(e));
+      };
+    
+    return (
+        <div class="container">
+            Hola world"
+            <button onClick={start} disabled={isRecording}>
+                Record
+            </button>
+            <button onClick={stop} disabled={!isRecording}>
+                Stop
+            </button>
+            <audio src={blobURL} controls="controls" />
+        </div>
+    );
 }
 
-export default consumerForm;
+function name(params) {
+
+}
+
+export default ConsumerForm;
